d94eab394adcd5ed13d83a935d99919efb920644
shravandeshpande1508 <shravan.deshpande@extrapreneursindia.com>
2021-10-01 19:32:51 +0530

    PGR labels

--- configs/egov-localization/localization_pgr_new_10032021.sql
+++ configs/egov-localization/localization_pgr_new_10032021.sql
@@ -4,4 +4,6 @@ INSERT INTO message (id,locale,code,message,tenantid,"module",createdby,createdd
 ('4d3b6355-1abb-5618-ac6f-2bc063b75064e','en_IN','CS_ADDCOMPLAINT_COMPLAINT_DETAILS_VALIDATION','Enter valid complaint additional details','ch','rainmaker-pgr',24226,now(),NULL,NULL)
 ,('4d3b6355-1abb-5619-ac6f-2bc063b75064e','en_IN','CS_ADDCOMPLAINT_LANDMARK_VALIDATION','Enter valid landmark','ch','rainmaker-pgr',24226,now(),NULL,NULL)
 ,('4d3b6355-1abb-5620-ac6f-2bc063b75064e','en_IN','CS_HOUSE_NO_ERROR_MESSAGE_VALIDATION','Enter valid House no','ch','rainmaker-pgr',24226,now(),NULL,NULL)
-,('4d3b6355-1abb-5621-ac6f-2bc063b75064e','en_IN','CORE_COMMON_INVALID_FILE_EXTENSION','Please select valid file!','ch','rainmaker-common',24226,now(),NULL,NULL);
+,('4d3b6355-1abb-5621-ac6f-2bc063b75064e','en_IN','CORE_COMMON_INVALID_FILE_EXTENSION','Please select valid file!','ch','rainmaker-common',24226,now(),NULL,NULL)
+,('92478367-1717-406f-8156-42397df7c275','en_IN','SERVICEDEFS.DEP017_ADVERTISEMENT_04',' Illegal Advertisement','ch','rainmaker-common',24226,now(),NULL,NULL)
+,('8af7c5b1-ee26-4e50-b95d-ae25789a947a','en_IN','SERVICEDEFS.DEP017_ADVERTISEMENT_05','Defacement','ch','rainmaker-common',24226,now(),NULL,NULL);
